97f99590b2daa99fe0746b61af5bd2cc67eb7474
ahegde3 <ahegde3@outlook.com>
2023-12-08 15:39:37 -0500

    knex file change

--- config/knexfile.js
+++ config/knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: process.env.host,
+      host: "", //Fill db ip address
       port: "3306",
-      user: process.env.username,
-      password: process.env.password,
+      user: "", //Fill user name
+      password: "", //Fill the password
       database: "job_application_portal",
     },
     migrations: {
